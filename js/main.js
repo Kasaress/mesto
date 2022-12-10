acec25d8d65dfa19ebad696b3c72996dc388d391
Яна Бубнова <bujhvh@mail.ru>
2022-12-10 20:07:45 +0300

    final

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
 const popUp = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+const submitButton = document.querySelector('.popup__submit-button');
+let formElements = document.querySelectorAll('.popup__input')
+
 function openPopup(popup) {
     popup.classList.add('popup_opened');
-    console.log('меня вызвали')
 };
-const closeButton = document.querySelector('.popup__close-button');
 function closePopup(popup) {
     popup.classList.remove('popup_opened');
 };
+function savePopup(popup) {
+    document.querySelector('.profile__name').textContent = formElements[0].value;
+    document.querySelector('.profile__description').textContent = formElements[1].value;
+    closePopup(popup)
+};
 editButton.addEventListener('click', (event) => {
-    openPopup(popUp)
+    openPopup(popUp);
+    formElements[0].value = document.querySelector('.profile__name').textContent;
+    formElements[1].value = document.querySelector('.profile__description').textContent;
 });
 closeButton.addEventListener('click', (event) => {
     closePopup(popUp)
 });
-
-
-
+submitButton.addEventListener('click', (event) => {
+    savePopup(popUp)
+});
